fa0e773af0ecfd050a88d10aee1a95c2fbf3745d
DotShark <arnaud.petit4@gmail.com>
2025-12-12 15:14:03 +0100

    feat: initialize Nuxt 4 project with Tailwind CSS, Pinia, Nuxt Fonts, and Nuxt Icon

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+import tailwindcss from '@tailwindcss/vite';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@pinia/nuxt',
+  ],
+  css: ['./app/assets/css/main.css'],
+  fonts: {
+    families: [
+      {
+        name: 'Inter',
+        provider: 'google',
+        weights: [400, 500, 600, 700],
+      },
+      {
+        name: 'Poppins',
+        provider: 'google',
+        weights: [400, 500, 600, 700],
+      },
+    ],
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  }
+})
